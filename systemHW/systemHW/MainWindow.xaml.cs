33a2ca8491bd8166fec8869ed349b11c86e48c9b
maksimKHLV <maksimxelilov@gmail.com>
2023-05-02 02:11:48 +0400

    system

---
+++ systemHW/systemHW/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Diagnostics;
+
+using System.Windows;
+
+
+namespace systemHW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ClickForOpenApp(object sender, RoutedEventArgs e)
+        {
+            
+            Process process = new Process();
+
+            
+            process.StartInfo.FileName = "steam.exe";
+
+            
+            process.Start();
+
+            
+            process.WaitForExit();
+
+            
+            MessageBox.Show($"Дочерний процесс завершен с кодом {process.ExitCode}");
+        }
+    }
+}
