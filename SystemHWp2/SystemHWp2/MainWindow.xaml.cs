8875fcaf59481f5d02603b92090fb3f5b9df4d16
maksimKHLV <maksimxelilov@gmail.com>
2023-05-02 02:13:22 +0400

    system

---
+++ SystemHWp2/SystemHWp2/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Diagnostics;
+
+using System.Windows;
+
+
+namespace SystemHWp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ClickForOpenApp(object sender, RoutedEventArgs e)
+        {
+            
+            Process process = new Process();
+
+
+            
+            process.StartInfo.FileName = "notepad.exe";
+
+            process.Start();
+            
+
+            MessageBoxButton buttons = MessageBoxButton.YesNo;
+            MessageBoxResult result = MessageBox.Show("Ожидать завершения?", "Выбор режима ожидания", buttons);
+        
+
+            if (result == MessageBoxResult.Yes)
+            {
+                
+                process.WaitForExit();
+
+                
+                MessageBox.Show($"Дочерний процесс завершен с кодом {process.ExitCode}");
+            }
+            else
+            {
+                
+                process.Kill();
+                Environment.Exit(0);
+            }
+        }
+    }
+}
